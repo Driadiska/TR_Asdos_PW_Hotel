feb9a18158df76ccba4fd2eb2c0103d74afffa09
~ Priwidy <linggapriwiditya@gmail.com>
2025-11-29 18:35:44 +0700

    Progres Dua

---
+++ nav/nav.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="nav.css">
+</head>
+<body>
+     <div class="nav">
+      <div class="logo">
+        <img src="Logo UKSW.png" alt="Logo" />
+        <div>
+          <h1>BHLILZ</h1>
+          <p>Hotel Terjangkau Dan Aman</p>
+        </div>
+      </div>
+
+      <nav>
+        <ul>
+          <li><a href="Menu.php">HOME</a></li>
+          <li><a href="Produk.php">SEWA KAMAR</a></li>
+          <li><a href="Klien.php" class="active">DETAIL</a></li>
+          <li><a href="Kontak.php">KONTAK</a></li>
+        </ul>
+      </nav>
+
+      <button class="cta">
+        <a href="Login.php" style="color:white; text-decoration:none;">Log Out</a>
+      </button>
+    </div>
+</body>
+</html> 
